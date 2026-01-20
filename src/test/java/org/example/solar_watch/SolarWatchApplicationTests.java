76cca4218fce33b8d9f00ec4d83d8b28eca7cbf9
Kristian Fredrik Lynum <kristian.lynum@no.experis.com>
2026-01-20 12:18:12 +0100

    init

---
+++ src/test/java/org/example/solar_watch/SolarWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.solar_watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
