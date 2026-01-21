9bbbaab38a8535f787aa58639d62e029ab123dda
Kristian Fredrik Lynum <kristian.lynum@no.experis.com>
2026-01-21 08:42:09 +0100

    able to use unsafe api

--- src/main/java/org/example/solar_watch/configuration/RestTemplateConfiguration.java
+++ src/main/java/org/example/solar_watch/configuration/RestTemplateConfiguration.java
@@ -1,4 +1,32 @@
 package org.example.solar_watch.configuration;
 
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+
+@Configuration
 public class RestTemplateConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate() throws Exception {
+
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }
+        };
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+
+        return new RestTemplate(new SimpleClientHttpRequestFactory());
+    }
 }
