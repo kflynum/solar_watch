3aaf301ea01f358096b3b5a8bdc829b282a13d04
Kristian Fredrik Lynum <kristian.lynum@no.experis.com>
2026-01-21 14:16:25 +0100

    Added some finds

--- src/main/java/org/example/solar_watch/repository/SunriseSunsetRepository.java
+++ src/main/java/org/example/solar_watch/repository/SunriseSunsetRepository.java
@@ -1,8 +1,16 @@
 package org.example.solar_watch.repository;
 
+import org.example.solar_watch.model.City;
+import org.example.solar_watch.model.SunriseSunset;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SunriseSunsetRepository extends JpaRepository {
+    List<SunriseSunset> findByCity(Long cityId);
+    Optional<SunriseSunset> findByCityIdAndDate(Long cityId, LocalDate date);
 }
